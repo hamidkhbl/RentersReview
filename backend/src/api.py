aceb0e38e76eec6e67307e782e256b34ac0e0536
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2022-11-11 15:05:46 -0800

    Add place routes

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
+from .models.place import Place
 
 from .models.base import setup_db
 from .models.user import User
@@ -7,7 +8,7 @@ app = Flask(__name__)
 setup_db(app)
 
 @app.route('/')
-def method_name():
+def index():
     return "not implemented"
 
 # Returns users from Auth0
@@ -22,3 +23,50 @@ def get_users():
         })
     except:
         abort(500)
+        
+@app.route('/places')
+def get_places():
+    places = Place.get_all_formatted()
+    return jsonify({
+        'success': True,
+        'places': places
+    })
+    
+@app.route('/places', methods=['POST'])
+def add_place():
+    place = Place(
+        title = request.json.get('title'),
+        address = request.json.get('address'),
+        city = request.json.get('city'),
+        state = request.json.get('state'),
+        description = request.json.get('description'),
+        latitude = request.json.get('latitude'),
+        longitude = request.json.get('longitude')
+    )
+    
+    place.insert()
+    return jsonify({
+        'success': True,
+        'place': place.format()
+    })
+    
+@app.route('/places/<int:place_id>', methods=['DELETE'])
+def delete_place(place_id):
+    place = Place.get_by_id(place_id)
+    if place is None:
+        abort(404)
+    else:
+        place.delete()
+    return jsonify({
+        'success': True,
+        'place': place.format()
+    })
+
+@app.route('/delete_place_bulk', methods=['POST'])
+def delete_places_bulk():
+    ids = request.json.get('ids')
+    result = Place.delete_bulk(ids)
+    return jsonify({
+        'deleted':result[0],
+        'error_occured': result[1]
+    })
