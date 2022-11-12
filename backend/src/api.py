f5ba677408863346282d6db8c9d3154d98d23936
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2022-11-11 16:31:02 -0800

    Add comment routes

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,5 +1,7 @@
+import datetime
 from flask import Flask, jsonify, abort, request
 from .models.place import Place
+from .models.comment import Comment
 
 from .models.base import setup_db
 from .models.user import User
@@ -7,12 +9,17 @@ from .models.user import User
 app = Flask(__name__)
 setup_db(app)
 
+
 @app.route('/')
 def index():
     return "not implemented"
 
 # Returns users from Auth0
 # Needs the super admin previlage
+
+# region user
+
+
 @app.route('/users')
 def get_users():
     try:
@@ -23,7 +30,8 @@ def get_users():
         })
     except:
         abort(500)
-        
+
+
 @app.route('/places')
 def get_places():
     places = Place.get_all_formatted()
@@ -31,25 +39,30 @@ def get_places():
         'success': True,
         'places': places
     })
-    
+# endregion
+
+# region place
+
+
 @app.route('/places', methods=['POST'])
 def add_place():
     place = Place(
-        title = request.json.get('title'),
-        address = request.json.get('address'),
-        city = request.json.get('city'),
-        state = request.json.get('state'),
-        description = request.json.get('description'),
-        latitude = request.json.get('latitude'),
-        longitude = request.json.get('longitude')
+        title=request.json.get('title'),
+        address=request.json.get('address'),
+        city=request.json.get('city'),
+        state=request.json.get('state'),
+        description=request.json.get('description'),
+        latitude=request.json.get('latitude'),
+        longitude=request.json.get('longitude')
     )
-    
+
     place.insert()
     return jsonify({
         'success': True,
         'place': place.format()
     })
-    
+
+
 @app.route('/places/<int:place_id>', methods=['DELETE'])
 def delete_place(place_id):
     place = Place.get_by_id(place_id)
@@ -62,11 +75,50 @@ def delete_place(place_id):
         'place': place.format()
     })
 
+
 @app.route('/delete_place_bulk', methods=['POST'])
 def delete_places_bulk():
     ids = request.json.get('ids')
     result = Place.delete_bulk(ids)
     return jsonify({
-        'deleted':result[0],
+        'deleted': result[0],
         'error_occured': result[1]
     })
+# endregion
+
+# region comment
+
+
+@app.route('/places/<int:place_id>/comments')
+def place_cooments(place_id):
+    comments = Comment.get_place_comments_formatted(place_id)
+    return jsonify({
+        'success': True,
+        'comments': comments
+    })
+
+
+@app.route('/places/<int:place_id>/comments', methods=['POST'])
+def add_comment_for_place(place_id):
+    comment = Comment(
+        title=request.json.get('title'),
+        creation_date=str(datetime.datetime.now()),
+        description=request.json.get('title'),
+        place_id=place_id,
+        user_id=0
+    )
+    comment.insert()
+    return jsonify({
+        'success': True,
+        'comment': comment.format()
+    })
+
+# endregion
+
+# region like
+
+
+@app.route('/place/<int:place_id>/comments/<int:comment_id>')
+def comment_likes(place_id, comment_id):
+    return "not implemented"
+# endregion
