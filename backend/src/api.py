94bda91390e1ffafd17c9483dd54aeab35b5c21a
Hamid Khodabandehloo <hamidkhbl@gmail.com>
2022-11-12 14:03:32 -0800

    Improve exception handling

--- backend/src/api.py
+++ backend/src/api.py
@@ -6,9 +6,12 @@ from .models.like import Like
 from .models.base import setup_db
 from .models.user import User
 from .auth.auth import requires_auth
+import logging
 
 app = Flask(__name__)
 setup_db(app)
+print(str(datetime.date.today))
+logging.basicConfig(filename='logs/{}.log'.format(datetime.date.today().strftime('%Y-%m-%d')), level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 
 @app.route('/')
@@ -29,7 +32,8 @@ def get_users():
             'success': True,
             'users': users
         })
-    except:
+    except Exception as e:
+        app.logger.error(e)
         abort(500)
 
 @app.route('/users/import')
@@ -40,7 +44,8 @@ def import_users():
             'success': True,
             'added': [u.format() for u in result[0]]
         })
-    except:
+    except Exception as e:
+        app.logger.error(e)
         abort(500)
 
 # endregion
@@ -49,65 +54,84 @@ def import_users():
 
 @app.route('/places')
 def get_places():
-    places = Place.get_all_formatted()
-    return jsonify({
-        'success': True,
-        'places': places
-    })
+    try:
+        places = Place.get_all_formatted()
+        return jsonify({
+            'success': True,
+            'places': places
+        })
+    except Exception as e:
+        app.logger.error(e)
+        abort(500)
     
 @app.route('/places', methods=['POST'])
 @requires_auth(permission='read:place')
 def add_place(user_id):
-    place = Place(
-        title=request.json.get('title'),
-        address=request.json.get('address'),
-        city=request.json.get('city'),
-        state=request.json.get('state'),
-        description=request.json.get('description'),
-        latitude=request.json.get('latitude'),
-        longitude=request.json.get('longitude'),
-        user_id=user_id
-    )
+    try:
+        place = Place(
+            title=request.json.get('title'),
+            address=request.json.get('address'),
+            city=request.json.get('city'),
+            state=request.json.get('state'),
+            description=request.json.get('description'),
+            latitude=request.json.get('latitude'),
+            longitude=request.json.get('longitude'),
+            user_id=user_id
+        )
 
-    place.insert()
-    return jsonify({
-        'success': True,
-        'place': place.format()
-    })
-
+        place.insert()
+        return jsonify({
+            'success': True,
+            'place': place.format()
+        })
+    except Exception as e:
+        app.logger.error(e)
+        abort(500)
 
 @app.route('/places/<int:place_id>', methods=['DELETE'])
 @requires_auth('read:place')
 def delete_place(user_id, place_id):
-    place = Place.get_by_id(place_id)
-    if place.user_id != user_id:
-        abort(403)
-    if place is None:
-        abort(404)
-    else:
-        place.delete()
-    return jsonify({
-        'success': True,
-        'place': place.format()
-    })
+    try:
+        place = Place.get_by_id(place_id)
+        if place.user_id != user_id:
+            abort(403)
+        if place is None:
+            abort(404)
+        else:
+            place.delete()
+        return jsonify({
+            'success': True,
+            'place': place.format()
+        })
+    except Exception as e:
+        app.logger.error(e)
+        abort(500)
 
 
 @app.route('/delete_place_bulk', methods=['POST'])
 def delete_places_bulk():
-    ids = request.json.get('ids')
-    result = Place.delete_bulk(ids)
-    return jsonify({
-        'deleted': result[0],
-        'error_occured': result[1]
-    })
+    try:
+        ids = request.json.get('ids')
+        result = Place.delete_bulk(ids)
+        return jsonify({
+            'deleted': result[0],
+            'error_occured': result[1]
+        })
+    except Exception as e:
+        app.logger.error(e)
+        abort(500)
 
 @app.route('/places/<int:place_id>')
 def get_place_by_id(place_id):
-    place = Place.get_by_id(place_id)
-    return jsonify({
-        'success': True,
-        'place': place.format_with_comments()
-    })
+    try:
+        place = Place.get_by_id(place_id)
+        return jsonify({
+            'success': True,
+            'place': place.format_with_comments()
+        })
+    except Exception as e:
+        app.logger.error(e)
+        abort(500)
 # endregion
 
 # region comment
@@ -115,28 +139,35 @@ def get_place_by_id(place_id):
 
 @app.route('/places/<int:place_id>/comments')
 def place_cooments(place_id):
-    comments = Comment.get_place_comments_formatted(place_id)
-    return jsonify({
-        'success': True,
-        'comments': comments
-    })
-
+    try:
+        comments = Comment.get_place_comments_formatted(place_id)
+        return jsonify({
+            'success': True,
+            'comments': comments
+        })
+    except Exception as e:
+        app.logger.error(e)
+        abort(500)
 
 @app.route('/comments', methods=['POST'])
 @requires_auth(permission='read:place')
 def add_comment_for_place(user_id):
-    comment = Comment(
-        title=request.json.get('title'),
-        creation_date=str(datetime.datetime.now()),
-        description=request.json.get('title'),
-        place_id=request.json.get('place_id'),
-        user_id=user_id
-    )
-    comment.insert()
-    return jsonify({
-        'success': True,
-        'comment': comment.format()
-    })
+    try:
+        comment = Comment(
+            title=request.json.get('title'),
+            creation_date=str(datetime.datetime.now()),
+            description=request.json.get('title'),
+            place_id=request.json.get('place_id'),
+            user_id=user_id
+        )
+        comment.insert()
+        return jsonify({
+            'success': True,
+            'comment': comment.format()
+        })
+    except Exception as e:
+        app.logger.error(e)
+        abort(500)
 
 # endregion
 
@@ -145,15 +176,19 @@ def add_comment_for_place(user_id):
 @app.route('/like', methods=['POST'])
 @requires_auth('read:place')
 def like(user_id):
-    comment_id=request.json.get('comment_id')
-    like = Like(
-        comment_id=comment_id,
-        user_id=user_id
-        )
-    result = like.like()
-    return jsonify({
-            'success': True,
-            'like': result
-        })
+    try:
+        comment_id=request.json.get('comment_id')
+        like = Like(
+            comment_id=comment_id,
+            user_id=user_id
+            )
+        result = like.like()
+        return jsonify({
+                'success': True,
+                'like': result
+            })
+    except Exception as e:
+        app.logger.error(e)
+        abort(500)
         
 # endregion
